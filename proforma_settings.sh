01723e2200f2fa962ebb9467fc88e0704277ea8f
Zaid <108116182+ZdAkk@users.noreply.github.com>
2024-04-09 05:54:28 +0200

    Create proforma_settings.sh

---
+++ proforma_settings.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Grader settings
+export SERVICE_URL=""
+export LMS_ID=""
+export LMS_PASSWORD=""
+export GRADER_NAME="Graja"
+export GRADER_VERSION="2.3"
+
+# Submission settings
+export FEEDBACK_TYPE="merged-test-feedback"
+export FEEDBACK_STRUCTURE="zip"
+export STUDENT_FEEDBACK_LEVEL="info"
+export TEACHER_FEEDBACK_LEVEL="debug"
+
+
+
