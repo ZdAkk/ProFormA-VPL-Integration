021e32be729101fadd36bbb991b234fb17a0f09f
Zaid <108116182+ZdAkk@users.noreply.github.com>
2024-04-10 17:54:49 +0200

    Update proforma_settings.sh

--- proforma_settings.sh
+++ proforma_settings.sh
@@ -4,14 +4,14 @@
 export SERVICE_URL=""
 export LMS_ID=""
 export LMS_PASSWORD=""
-export GRADER_NAME="Graja"
-export GRADER_VERSION="2.3"
+export GRADER_NAME=""
+export GRADER_VERSION=""
 
 # Submission settings
-export FEEDBACK_TYPE="merged-test-feedback"
-export FEEDBACK_STRUCTURE="zip"
-export STUDENT_FEEDBACK_LEVEL="info"
-export TEACHER_FEEDBACK_LEVEL="debug"
+export FEEDBACK_TYPE=""
+export FEEDBACK_STRUCTURE=""
+export STUDENT_FEEDBACK_LEVEL=""
+export TEACHER_FEEDBACK_LEVEL=""
 
 
 
